25797fc7069d7b1167c568b45d5ad886b6851625
Elena Pedko <elpg.ods@gmail.com>
2024-02-14 15:41:02 +0200

    styles for poject list are added

--- src/components/styles/ProjectsPage.styled.js
+++ src/components/styles/ProjectsPage.styled.js
@@ -3,24 +3,59 @@ import styled from 'styled-components';
 export const List = styled.ul`
     display: flex;
     flex-wrap: wrap;
-    margin: -30px;
+    margin: -20px;
   
 `;
 
 export const Item = styled.li`
     width: 100%;
-    margin: 30px;
+    margin: 20px;
 
     @media screen and (min-width: 768px) {
-        width: calc((100% - 4 * 30px) / 2); 
+        width: calc((100% - 4 * 20px) / 2); 
     }
 
     @media screen and (min-width: 1280px) {
-        width: calc((100% - 6 * 30px) / 3);    
+        width: calc((100% - 6 * 20px) / 3);    
     }
 `;
 
 export const Wrapper = styled.div`
     padding: 20px;
     background-color: #fff;
+`;
+
+const text = `
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 1.14;
+    letter-spacing: 0.02em;
+    color: var(--text-color);
+    text-align: justify;
+    margin-bottom: 10px;
+
+    @media screen and (min-width: 768px) {
+        font-size: 14px;
+    }
+
+    @media screen and (min-width: 1280px) {
+        font-size: 16px;    
+    }
+`;
+
+export const Title = styled.p`
+    ${text}
+    font-weight: 600;
+
+`;
+
+export const Description = styled.span`
+    ${text}
+`;
+
+export const Link = styled.a`
+    display: inline-block;
+    ${text}
+    font-weight: 600;
+    margin-bottom: 10px;
 `
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,12 @@
 import { Section } from '../components/styles/Section.styled';
 import { Container } from '../components/styles/Container.styled';
-import { List, Item, Wrapper } from '../components/styles/ProjectsPage.styled';
+import { List, Item, Wrapper, Title, Description, Link } from '../components/styles/ProjectsPage.styled';
 
 import Projects from '../data/projects.json';
 
 
 const Project = () => {
+
   return (
     <Section>
       <Container>
@@ -17,15 +18,14 @@ const Project = () => {
         <Item key={id}>
           <img src={image} alt={title} />
           <Wrapper>
-          <p>Title: <span>{title}</span></p>
-          <p>Description: <span>{description}</span></p>
-          <p>Project type: <span>{type}</span></p>
-          <p>Stack: <span>{technologies}</span></p>
-          {github ? <a href={github}>link to Github</a> : <p>Link to Github: not available</p>}
-          <p><a href={livePage}>Link to live page</a></p>
+          <Title>Title: <Description>{title}</Description></Title>
+          <Title>Description: <Description>{description}</Description></Title>
+          <Title>Project type: <Description>{type}</Description></Title>
+          <Title>Stack: <Description>{technologies.join(', ')}</Description></Title>
+          {github ? <Link href={github} rel="noopener noreferrer nofollow" target='_blank' aria-label="link to Github">Link to Github</Link> : <Title>Link to Github: <Description>not available</Description></Title>}
+          <Title><Link href={livePage} rel="noopener noreferrer nofollow" target='_blank' aria-label="link to Live page">Link to live page</Link></Title>
           </Wrapper>
-          </Item>
-          
+          </Item>  
         )
        })}
        
